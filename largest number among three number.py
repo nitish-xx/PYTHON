6ea5ba884a6b7e08b81625157dbaf49f57da98f0
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-06-22 22:55:25 +0530

    Update and rename largest number among three number to largest number among three number.py
    This program should be written like this :)

--- largest number among three number.py
+++ largest number among three number.py
@@ -3,10 +3,12 @@
 num1 = 80
 num2 = 8
 num3 = 16
+
 if (num1 >=num2) and (num1 >=num3):
-largest = num1
+  largest = num1
 elif(num2 >=num1) and (num2 >=num3):
-largest = num2
+  largest = num2
 else:
-largest = num3
+  largest = num3
+  
 print("The largest number is", largest)
